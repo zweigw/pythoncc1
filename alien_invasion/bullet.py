d4b590fa30ac0a5663900874b507b3fa0b519924
Zweig <na@na.com>
2017-01-07 00:44:48 +0100

    Files per submission of Reddit thread

---
+++ alien_invasion/bullet.py
@@ -0,0 +1,39 @@
+#! /usr/bin/env python3
+# bullet.py
+
+import pygame
+from pygame.sprite import Sprite
+
+class Bullet(Sprite):
+    """A class to manage bullets fired from the ship"""
+
+    def __init__(self, ai_settings, screen, ship):
+        """Create a bullet object at the ship's current position."""
+        super().__init__()
+        # The call super(Bullet, self).__init__() uses Python 2.7 syntax. This 
+        # works in Python 3 too, or you can also write this call more simply
+        # as super().__init__().
+        self.screen = screen
+
+        # Create a bullet rect at (0, 0) and then set correct position.
+        self.rect = pygame.Rect(0, 0, ai_settings.bullet_width,
+            ai_settings.bullet_height)
+        self.rect.centerx = ship.rect.centerx
+        self.rect.top = ship.rect.top
+
+        # Store the bullet's position as a decimal value
+        self.y = float(self.rect.y)
+
+        self.color = ai_settings.bullet_color
+        self.speed_factor = ai_settings.bullet_speed_factor
+
+    def update(self):
+        """Move the bullet up the screen."""
+        # Update the decimal position of the bullet.
+        self.y -= self.speed_factor
+        # Update the rect position.
+        self.rect.y = self.y
+
+    def draw_bullet(self):
+        """Draw the bullet to the screen"""
+        pygame.draw.rect(self.screen, self.color, self.rect)
